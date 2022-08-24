10dbd6b197e17ab489b31dc5e3cad81d1fb86589
Rendyaditia <109193089+Rendyaditia@users.noreply.github.com>
2022-08-24 15:07:31 +0700

    Add files via upload

---
+++ fungsivolumebalok_rendy.php
@@ -0,0 +1,11 @@
+<?php
+// pembuatan fungsi
+$p =18;
+$l =10;
+$t =12;
+function volumebalok ($p,$l,$t) {
+    return ($p*$l*$t);
+}
+// menampilan fungsi
+echo "volume fungsi tersebut adalah:" .volumebalok($p,$l,$t). "cm";
+?>
